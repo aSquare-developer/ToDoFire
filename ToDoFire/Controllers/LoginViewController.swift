baeb3c606eb083fe2955ef661550371d0de7ca9e
Artur <artur.anissimov@gmail.com>
2020-02-11 18:33:10 +0200

    Creating tasks screen

---
+++ ToDoFire/Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ToDoFire
+//
+//  Created by Artur Anissimov on 11.02.2020.
+//  Copyright © 2020 Artur Anissimov. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
+    
+    
+}
+
